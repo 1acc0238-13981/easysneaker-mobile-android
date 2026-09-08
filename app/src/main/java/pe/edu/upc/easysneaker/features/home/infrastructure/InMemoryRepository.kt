fd4dd24007a5f3b75b3fb5c96e5880c6a52566e1
jmayta@pucp.edu.pe <jmayta@pucp.edu.pe>
2026-09-08 14:15:24 -0500

    feat: implement in-memory product repository and product repository interface

---
+++ app/src/main/java/pe/edu/upc/easysneaker/features/home/infrastructure/InMemoryRepository.kt
@@ -0,0 +1,26 @@
+package pe.edu.upc.easysneaker.features.home.infrastructure
+
+import pe.edu.upc.easysneaker.features.home.domain.Product
+import pe.edu.upc.easysneaker.features.home.domain.ProductRepository
+
+class InMemoryRepository : ProductRepository {
+    val products = listOf(
+        Product(
+            id = 1,
+            name = "Product 1",
+            price = 100.0,
+            imageUrl = "https://i.imgur.com/E8ZBAnC.png"
+        ),
+        Product(
+            id = 2,
+            name = "Product 2",
+            price = 150.0,
+            imageUrl = "https://i.imgur.com/E8ZBAnC.png"
+        )
+    )
+
+    override fun getProducts(): List<Product> {
+        return products
+    }
+
+}
