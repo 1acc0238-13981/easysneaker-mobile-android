748dc31897df5042d53b9720b6f6b4d754f1ff7c
jmayta@pucp.edu.pe <jmayta@pucp.edu.pe>
2026-09-08 14:02:28 -0500

    feat: enhance product card layout with image resizing and price display

--- app/src/main/java/pe/edu/upc/easysneaker/HomeScreen.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/HomeScreen.kt
@@ -1,6 +1,8 @@
 package pe.edu.upc.easysneaker
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -8,6 +10,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
@@ -32,10 +35,19 @@ fun HomeScreen() {
 
     LazyColumn {
         items(products) { product ->
-            Card(modifier = Modifier.padding(8.dp)) {
-                Column {
-                    AsyncImage(model = product.imageUrl, contentDescription = product.name)
-                    Text(text = product.name, modifier = Modifier.padding(8.dp))
+            Card(modifier = Modifier.fillMaxWidth().padding(8.dp)) {
+                Column (modifier = Modifier.padding(8.dp)){
+                    AsyncImage(
+                        model = product.imageUrl,
+                        contentDescription = product.name,
+                        modifier = Modifier.fillMaxWidth().height(200.dp))
+                    Text(
+                        text = product.name,
+                        fontWeight = FontWeight.Bold,)
+
+                    Text(
+                        text = "$ ${product.price}"
+                    )
 
                 }
             }
