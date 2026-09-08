b9394e74ad441439e43d44c2bfeb9b5b7a37ce95
jmayta@pucp.edu.pe <jmayta@pucp.edu.pe>
2026-09-08 13:45:10 -0500

    chore: remove Glide dependencies from build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation("com.github.bumptech.glide:glide:4.14.2")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
