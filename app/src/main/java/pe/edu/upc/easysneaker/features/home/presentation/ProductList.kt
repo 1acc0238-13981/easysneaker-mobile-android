c027559e4967ba950e1ead659c9b063d490da8cc
jmayta@pucp.edu.pe <jmayta@pucp.edu.pe>
2026-09-08 15:39:54 -0500

    chore: clean up unused imports and remove redundant color schemes from Theme.kt

--- app/src/main/java/pe/edu/upc/easysneaker/features/home/presentation/ProductList.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/features/home/presentation/ProductList.kt
@@ -1,19 +1,10 @@
 package pe.edu.upc.easysneaker.features.home.presentation
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
 import pe.edu.upc.easysneaker.features.home.domain.Product
 
 @Composable
