95ea05b80cad016e8ff91eb7ac1091e5fa9276d9
jmayta@pucp.edu.pe <jmayta@pucp.edu.pe>
2026-09-10 15:18:20 -0500

    feat(home): update GetProductsUseCase to use ProductRepositoryImpl for product retrieval

--- app/src/main/java/pe/edu/upc/easysneaker/features/home/application/GetProductsUseCase.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/features/home/application/GetProductsUseCase.kt
@@ -3,8 +3,9 @@ package pe.edu.upc.easysneaker.features.home.application
 import pe.edu.upc.easysneaker.features.home.domain.Product
 import pe.edu.upc.easysneaker.features.home.domain.ProductRepository
 import pe.edu.upc.easysneaker.features.home.infrastructure.repositories.InMemoryRepository
+import pe.edu.upc.easysneaker.features.home.infrastructure.repositories.ProductRepositoryImpl
 
-class GetProductsUseCase(private val repository: ProductRepository = InMemoryRepository()) {
+class GetProductsUseCase(private val repository: ProductRepository = ProductRepositoryImpl()) {
 
     suspend operator fun invoke(): List<Product> = repository.getProducts()
 }
--- app/src/main/java/pe/edu/upc/easysneaker/features/home/infrastructure/repositories/ProductRepositoryImpl.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/features/home/infrastructure/repositories/ProductRepositoryImpl.kt
@@ -3,10 +3,32 @@ package pe.edu.upc.easysneaker.features.home.infrastructure.repositories
 import pe.edu.upc.easysneaker.features.home.domain.Product
 import pe.edu.upc.easysneaker.features.home.domain.ProductRepository
 import pe.edu.upc.easysneaker.features.home.infrastructure.remote.ProductService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class ProductRepositoryImpl(private val service: ProductService): ProductRepository {
+class ProductRepositoryImpl(
+    private val service: ProductService
+    = Retrofit.Builder()
+        .baseUrl("https://sneakerapi-y3ub.onrender.com/api")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build().create(ProductService::class.java)
+) : ProductRepository {
 
     override suspend fun getProducts(): List<Product> {
+        val response = service.getProducts()
 
+        if (response.isSuccessful) {
+            response.body()?.let { productsResponseDto ->
+                return productsResponseDto.products.map { productDto ->
+                    Product(
+                        id = productDto.id,
+                        name = productDto.name,
+                        price = productDto.price,
+                        imageUrl = productDto.image
+                    )
+                }.toList()
+            }
+        }
+        return emptyList()
     }
 }
