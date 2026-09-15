07016335b495aede5b93fc33fc2a5b73034e8f32
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-15 15:38:08 -0500

    feat: enhance ProductDetailScreen with loading and error states

--- app/src/main/java/pe/edu/upc/easysneaker/features/home/presentation/ProductDetailScreen.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/features/home/presentation/ProductDetailScreen.kt
@@ -2,12 +2,23 @@ package pe.edu.upc.easysneaker.features.home.presentation
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import coil3.compose.AsyncImage
+import pe.edu.upc.easysneaker.core.visibility
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductDetailScreen(
     modifier: Modifier = Modifier,
@@ -15,9 +26,42 @@ fun ProductDetailScreen(
     viewModel: ProductDetailViewModel = hiltViewModel(),
     onBack: () -> Unit
 ) {
-    Scaffold(modifier = modifier) { paddingValues ->
-        Column(modifier = Modifier.padding(paddingValues)) {
+    val uiState = viewModel.uiState.collectAsState().value
 
+    LaunchedEffect(id) {
+        viewModel.loadProduct(id)
+    }
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(title = {}, navigationIcon = {
+                IconButton(onClick = onBack) {
+                    Icon(visibility, contentDescription = "back")
+                }
+            })
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues)
+
+        ) {
+            when (uiState) {
+                is ProductDetailUiState.Loading -> {
+                    CircularProgressIndicator()
+                }
+
+                is ProductDetailUiState.Success -> {
+                    AsyncImage(
+                        model = uiState.product.imageUrl,
+                        contentDescription = uiState.product.name
+                    )
+                }
+
+                is ProductDetailUiState.Error -> {
+                    Text(text = uiState.message)
+                }
+            }
         }
     }
 }
