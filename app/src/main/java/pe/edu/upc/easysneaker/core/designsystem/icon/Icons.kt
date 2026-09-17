f96a6e3d11e2e49b0376a2e2f497ed7c6669879b
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-17 14:57:43 -0500

    refactor: remove unnecessary visibility modifier from arrowBack property in Icons.kt

--- app/src/main/java/pe/edu/upc/easysneaker/core/designsystem/icon/Icons.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/core/designsystem/icon/Icons.kt
@@ -229,7 +229,7 @@ val visibilityOff: ImageVector
 private var _visibilityOff: ImageVector? = null
 
 @Suppress("CheckReturnValue")
-public val arrowBack: ImageVector
+val arrowBack: ImageVector
     get() {
         if (_arrowBack != null) {
             return _arrowBack!!
